fa31956b9f2cf7dcd86af823cb8c5a600c980bf7
kralljr <jenna.krall@gmail.com>
2021-03-19 13:08:34 -0400

    Update R part

--- setup-ec2.sh
+++ setup-ec2.sh
@@ -3,3 +3,7 @@ sudo amazon-linux-extras install "R4"
 sudo yum install clang
 sudo yum install curl-devel
 wget https://raw.githubusercontent.com/kralljr/gestdc-sa-stan/main/sim-run-base.R
+chmod +x sim-run-base.R
+wget https://github.com/kralljr/gestdc-sa-stan/blob/main/setup-ec2.R
+chmod +x setup-ec2.R
+Rscript setup-ec2.R
