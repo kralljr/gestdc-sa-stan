03583d2c13fef3c381573797727e258366cd0752
kralljr <jenna.krall@gmail.com>
2021-03-19 13:26:00 -0400

    Fix raw .R

--- setup-ec2.sh
+++ setup-ec2.sh
@@ -4,6 +4,6 @@ sudo yum install clang
 sudo yum install curl-devel
 wget https://raw.githubusercontent.com/kralljr/gestdc-sa-stan/main/sim-run-base.R
 chmod +x sim-run-base.R
-wget https://github.com/kralljr/gestdc-sa-stan/blob/main/setup-ec2.R
+wget https://raw.githubusercontent.com/kralljr/gestdc-sa-stan/main/setup-ec2.R
 chmod +x setup-ec2.R
 Rscript setup-ec2.R
